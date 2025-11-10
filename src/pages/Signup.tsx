293147a6cdceb187c45d13327e68a665b5e7f208
jashwanthr-git <jashwanthr975@gmail.com>
2025-11-10 21:56:47 +0530

    Remove mail rate limit warning from Signup page
    Removed warning message about mail rate limit and credentials.

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -100,12 +100,6 @@ const Signup = () => {
               <h2 className="text-xl font-bold text-foreground">Create account</h2>
               <p className="text-muted-foreground text-sm mt-1">
                 Sign up to start using EdGenie
-                <p  style={{color: "red"}} className="text-l text-foreground font-bold ">
-                **Mail rate limit reached. Please use the following credentials to sign in:, 
-                Email: jashwanthr975@gmail.com 
-                Passward: Jashwanth@123**
-                
-              </p>
               </p>
             </div>
             
